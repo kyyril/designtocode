f02a78b40edb118a50d8da686110df70c0d9d98c
kyyril <khairilrahman55@gmail.com>
2025-02-08 23:21:19 +0700

    feat:upload image and cancel

---
+++ app/(routes)/dashboard/_components/ImageUpload.tsx
@@ -0,0 +1,64 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { ArrowUp, CloudUpload, UploadIcon, X } from "lucide-react";
+import Image from "next/image";
+import React, { useState } from "react";
+
+function ImageUpload() {
+  const [previewUrl, setPreviewUrl] = useState<string | null>(null);
+
+  const onImageSelect = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const files = e.target.files;
+    if (files) {
+      const url = URL.createObjectURL(files[0]);
+      setPreviewUrl(url);
+    }
+  };
+  return (
+    <div className="mt-10">
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-10">
+        {!previewUrl ? (
+          <div className="rounded-md p-7 shadow-md flex justify-center items-center flex-col">
+            <CloudUpload className="h-10 w-10" />
+            <h2 className="text-lg font-bold">Upload Image</h2>
+            <p className="text-gray-400 p-4">Click Button To Select Image</p>
+            <div className="p-4 w-full mt-4 border rounded-md flex justify-center">
+              <label htmlFor="imageUpload">
+                <h2 className="text-sm text-secondary bg-primary rounded-md shadow-sm p-2 hover:opacity-90 cursor-pointer">
+                  Select Image
+                </h2>
+              </label>
+            </div>
+
+            <input
+              type="file"
+              id="imageUpload"
+              className="hidden"
+              multiple={false}
+              onChange={onImageSelect}
+            />
+          </div>
+        ) : (
+          <div className="p-5 border flex justify-center items-center flex-col rounded-md shadow-md">
+            <Image
+              width={500}
+              height={500}
+              src={previewUrl}
+              alt="previewImg"
+              className="object-contain w-full h-[300px]"
+            />
+            <X
+              className="cursor-pointer mt-2 h-10 w-10 border rounded-sm text-red-600 hover:text-red-300"
+              onClick={() => {
+                setPreviewUrl(null);
+              }}
+            />
+          </div>
+        )}
+        <div>Promt</div>
+      </div>
+    </div>
+  );
+}
+
+export default ImageUpload;
--- app/(routes)/dashboard/page.tsx
+++ app/(routes)/dashboard/page.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import ImageUpload from "./_components/ImageUpload";
 
 function Dashboard() {
-  return <div>Dashboard</div>;
+  return (
+    <div className="xl:px-20">
+      <h2 className="text-3xl font-bold">Convert Design To Code</h2>
+      <ImageUpload />
+    </div>
+  );
 }
 
 export default Dashboard;
