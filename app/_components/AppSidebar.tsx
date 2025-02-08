905b04e9544eeaea796b31e2a5974c795f78c586
kyyril <khairilrahman55@gmail.com>
2025-02-08 10:40:15 +0700

    style(sidebar): active sidebar

--- app/_components/AppSidebar.tsx
+++ app/_components/AppSidebar.tsx
@@ -8,77 +8,65 @@ import {
   SidebarHeader,
   SidebarMenu,
 } from "@/components/ui/sidebar";
-import { Calendar, Home, Inbox, Search, Settings } from "lucide-react";
+import { CoinsIcon, Home, PaintBucket } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const items = [
   {
-    title: "Home",
-    url: "#",
+    title: "Workspace",
+    url: "/dashboard",
     icon: Home,
   },
   {
-    title: "Inbox",
-    url: "#",
-    icon: Inbox,
+    title: "Design",
+    url: "/design",
+    icon: PaintBucket,
   },
   {
-    title: "Calendar",
-    url: "#",
-    icon: Calendar,
-  },
-  {
-    title: "Search",
-    url: "#",
-    icon: Search,
-  },
-  {
-    title: "Settings",
-    url: "#",
-    icon: Settings,
+    title: "Credits",
+    url: "/credits",
+    icon: CoinsIcon,
   },
 ];
 
 export function AppSidebar() {
+  const pathname = usePathname();
+  console.log(pathname);
   return (
     <Sidebar>
       <SidebarHeader>
-        <div className="p-4">
-          <Image
-            src={"./logo.svg"}
-            alt="logo"
-            width={100}
-            height={100}
-            className="w-full h-full"
-          />
-          <h2 className="text-sm text-gray-400 text-center">Build Awesome</h2>
-        </div>
+        <a
+          className="flex-none text-xl font-semibold dark:text-white"
+          href="#"
+          aria-label="Brand"
+        >
+          Brand
+        </a>
       </SidebarHeader>
       <SidebarContent>
         <SidebarGroup>
           <SidebarGroupContent>
             <SidebarMenu className="mt-5">
               {items.map((item, index) => (
-                // <SidebarMenuItem key={item.title} className='p-2'>
-                //     <SidebarMenuButton asChild className=''>
                 <a
                   href={item.url}
                   key={index}
-                  className="p-2 text-lg flex gap-2 items-center
-                                 hover:bg-gray-100 rounded-lg"
+                  className={`p-2 text-lg flex gap-2 items-center hover:bg-gray-100 rounded-lg ${
+                    pathname == item.url && "bg-gray-200"
+                  }`}
                 >
                   <item.icon className="h-5 w-5" />
                   <span>{item.title}</span>
                 </a>
-                //     </SidebarMenuButton>
-                // </SidebarMenuItem>
               ))}
             </SidebarMenu>
           </SidebarGroupContent>
         </SidebarGroup>
       </SidebarContent>
       <SidebarFooter>
-        <h2 className="p-2 text-gray-400 text-sm">Copyright @Tubeguruji</h2>
+        <h2 className="p-2 text-gray-400 text-sm">@kyyril</h2>
       </SidebarFooter>
     </Sidebar>
   );
